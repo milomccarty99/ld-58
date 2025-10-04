2f20060dd9e7d5e130da153f2df3f07b56c758e5
milomccarty99 <milomccarty99@gmail.com>
2025-10-03 19:10:39 -0700

    switched to tile map layer

--- scripts/environment.gd
+++ scripts/environment.gd
@@ -39,12 +39,15 @@ func _enter_tree() -> void:
 	var noise_texture = Noise.generate_scene_unique_id()
 	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
 	noise.seed = randi()
-	noise.frequency = 0.05
-	for i in range(0,50):
-		for j in range(0,50):
+	noise.frequency = .1
+	for i in range(0,2000):
+		for j in range(0,2000):
 			if noise.get_noise_2d(i,j) > .5:
-				var new_env = exampleenv.instantiate()
-				new_env.position.x = i * 32
-				new_env.position.y = j * 32
-				add_child(new_env)
+				$TileMapLayer.set_cell(Vector2(i,j),0,Vector2(0,0))
+				
+				# long way
+				#var new_env = exampleenv.instantiate()
+				#new_env.position.x = i * 32
+				#new_env.position.y = j * 32
+				#add_child(new_env)
 	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
-
+var energy = 10
 var speed = 100
 
+func _enter_tree() -> void:
+	position.x = 16 + 320
+	position.y = 16 + 320
+
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("up"):
-		position.y -= delta * speed
-	if Input.is_action_pressed("down"):
-		position.y += delta * speed
-	if Input.is_action_pressed("left"):
-		position.x -= delta * speed
-	if Input.is_action_pressed("right"):
-		position.x += delta * speed
+	if Input.is_action_just_pressed("up"):
+		position.y -= 32
+	if Input.is_action_just_pressed("down"):
+		position.y += 32
+	if Input.is_action_just_pressed("left"):
+		position.x -= 32
+	if Input.is_action_just_pressed("right"):
+		position.x += 32
 	print (" x,y {}{}", position.x, position.y)
 	pass
